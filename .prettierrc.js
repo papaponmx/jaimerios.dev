6a34851ebf2d95009a80d38559615290844093ea
papaponmx <jaime.rios.dev@outlook.com>
2020-09-18 18:56:17 -0500

    Added prettier and svelte configuration

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+// .prettierrc.js
+module.exports = {
+  svelteSortOrder: 'styles-markup-scripts',
+  svelteStrictMode: true,
+  svelteBracketNewLine: true,
+  svelteAllowShorthand: false,
+  plugins: ['prettier-plugin-svelte']
+}
