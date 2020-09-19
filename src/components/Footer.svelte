edeec114a34f9ed61d8bd1332bddf79f34776ee3
papaponmx <jaime.rios.dev@outlook.com>
2020-09-19 14:53:16 -0500

    BUGFIX: Background url.

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,12 +1,12 @@
 <style>
   footer {
-    color: aqua;
     padding: 20px;
     max-width: 22.22px;
-}
+  }
 </style>
 
+<footer>Copyright 2020 Jaime Rios</footer>
 
-<footer>
-  <!-- Copyright 2020 Jaime Rios -->
-</footer>
+<script lang="ts">
+  export const segment = "";
+</script>
