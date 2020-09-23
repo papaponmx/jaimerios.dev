f1e81585d10a5699dd6bd53f39e94fdc76e5a788
papaponmx <jaime.rios.dev@outlook.com>
2020-09-22 21:05:01 -0500

    Updated front page layout.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -6,7 +6,7 @@
   }
 
   h1 {
-    color: var(--primary-color);
+    color: var(--pomegranate-color);
     font-size: 4rem;
     font-weight: 100;
     margin-bottom: 0.25rem;
@@ -21,7 +21,7 @@
 
   h3 {
     font-weight: 700;
-    color: #333;
+    color: var(--mirage-color);
   }
 
   p {
@@ -39,16 +39,23 @@
   }
 
   a {
-    border: 0.0313rem solid var(--primary-color);
+    border-bottom: 1px solid var(--pomegranate-color);
+    font-weight: 600;
+    /* box-shadow: 0 0.5px var(--twine-color); */
+    /* border-radius: calc(var(--spacing-unit) / 4); */
+    color: var(--mirage-color);
     text-decoration: none;
-    border-radius: calc(var(--spacing-unit) / 4);
     padding: calc(var(--spacing-unit) / 2);
     margin-right: calc(var(--spacing-unit) / 2);
+  }
+
+  a:first-of-type {
+    margin-right: calc(var(--spacing-unit) * 2);
   }
   a:hover,
   a:focus {
     color: #ffffff;
-    box-shadow: inset 0 0 0 2rem var(--primary-color);
+    box-shadow: inset 0 0 0 2rem var(--pomegranate-color);
     transition-duration: 250ms;
   }
 </style>
@@ -69,7 +76,7 @@
   </header>
   <div class="cta-wrapper">
     <a href="portfolio">Portfolio</a>
-    <a href="blog">Ready my blog</a>
+    <a href="blog">Blog</a>
   </div>
 </main>
 
