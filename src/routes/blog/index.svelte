6db2714b0988eb9fd9a2df4f2dbcdce822639b65
papaponmx <jaime.rios.dev@outlook.com>
2020-09-30 22:39:24 -0500

    Updated blog/index  markup.

--- src/routes/blog/index.svelte
+++ src/routes/blog/index.svelte
@@ -2,6 +2,33 @@
   ul {
     margin: 0 0 1em 0;
     line-height: 1.5;
+    list-style-type: none;
+  }
+
+  li {
+    /* border: 0.1px solid #aaa; */
+    color: var(--mirage-color);
+    min-height: calc(100vh - 6.25rem);
+    margin-bottom: 1rem;
+    padding: 3rem;
+  }
+
+  li a {
+    text-decoration: none;
+  }
+
+  h2 {
+    font-size: 4.25rem;
+    font-family: Georgia, 'Times New Roman', Times, serif;
+    color: var(--mirage-color);
+    margin-bottom: 0;
+    font-weight: 200;
+  }
+
+  time {
+    color: var(--napa-color);
+    font-size: 1.5rem;
+    margin-top: -2rem;
   }
 </style>
 
@@ -9,36 +36,45 @@
   <title>Blog</title>
 </svelte:head>
 
-<h1>Recent posts</h1>
+<!-- <h1>Recent posts</h1> -->
 
 <ul>
   {#each posts as post}
-    <li><a rel="prefetch" href="blog/{post.slug}">{post.title}</a></li>
+    <li>
+      <a rel="prefetch" href="blog/{post.slug}">
+        <h2>{post.title}</h2>
+        <time>{new Date(post.published_at).toDateString()}</time>
+        <p>{post.description}</p>
+      </a>
+    </li>
   {/each}
 </ul>
 
 <script context="module">
-  export function preload({ params, query }, session) {
-    return this.fetch(`${session.BLOG_API_URL}/articles?username=papaponmx`)
+  // const getNextFivePosts = () => {
+  //   if (process.browser) {
+  //     fetch('blog/new_posts');
+  //   }
+  // };
+
+  export function preload({ _params, _query }, session) {
+    const { BLOG_NEW_POSTS_URL } = session;
+    return this.fetch(BLOG_NEW_POSTS_URL)
       .then(r => r.json())
       .then(posts => {
         return { posts };
       });
   }
 
+  // TODO: Add Intersection Observer API implementation
   // What do I need to do?
-  // 1. Set initial blog post to 0
-  // 2. Get the next 5 blog posts
-  // 3. Update that markup.
-  //   4. Get the previous 5
-  //
-  //
-  //
-  //
-  //
-  //
+  // ✅ 1. Set initial blog post to 0
+  //  2. Get the next 5 blog posts
+  //  3. Update that markup.
 </script>
 
-<script lang="ts">
+<script>
+  import { each } from 'svelte/internal';
+
   export let posts;
 </script>
