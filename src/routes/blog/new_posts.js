90e2b42ac5aafc8dea43f1df723a968bb7bab6e1
papaponmx <jaime.rios.dev@outlook.com>
2020-09-30 22:38:56 -0500

    Formatting

--- src/routes/blog/new_posts.js
+++ src/routes/blog/new_posts.js
@@ -8,7 +8,7 @@ export async function get(req, res) {
   const USERNAME = 'papaponmx';
 
   // TODO: Implement req.params.currentPage
-  const URL = `${BLOG_API_URL}/articles?username=${USERNAME}&page=${CURRENT_PAGE}&per_page=${PER_PAGE}`;
+  const URL = `${BLOG_API_URL}/articles?username=${USERNAME}&page=${CURRENT_PAGE}&per_page=${PER_PAGE}&top=5`;
   fetch(URL)
     .then(r => r.json())
     .then(posts => {
