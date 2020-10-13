0e8ab4a54c2be745fcc1851fafc6eac712b62d94
papaponmx <jaime.rios.dev@outlook.com>
2020-10-13 18:01:40 -0500

    About page progress.

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,26 +1,59 @@
 <style>
   h1 {
     /* border-bottom: 0.2px solid var(--twine-color); */
-    color: var(--pomegranate-color);
-    font-size: calc(var(--base-font-size) * 3);
-    font-weight: 200;
-    max-width: 150px;
+    color: var(--mirage-accent-color);
+    font-size: calc(var(--base-font-size) * 7);
+    font-weight: 600;
   }
 
   section {
     color: var(--mirage-color);
     font-weight: 200;
   }
+
+  /* header {
+    border: 1px solid gray;
+  } */
+
+  hr {
+    background-color: var(--pomegranate-color--lighter);
+    border: none;
+    height: 5px;
+    width: 30px;
+    margin-top: 9px;
+    margin-left: 0;
+    margin-right: 0;
+  }
+
+  .subheading--row {
+    display: flex;
+    flex-direction: row;
+    align-items: flex-start;
+    justify-content: flex-start;
+    margin-top: -4rem;
+  }
+
+  .subheading--row span {
+    text-indent: 10px;
+    /* margin-left: 5px; */
+    text-transform: uppercase;
+    letter-spacing: 0.75px;
+  }
 </style>
 
 <svelte:head>
-  <title>About</title>
+  <title>About me</title>
 </svelte:head>
-<h1>About</h1>
 
-<section>
-  <h3>
-    I'm a self-taught front end developer from Mexico, programming worldwide.
-  </h3>
-</section>
-<!-- <p>This is the 'about' page. There's not much here.</p> -->
+<header>
+  <h1>About me</h1>
+  <div class="subheading--row">
+    <hr />
+    <span>
+      <!-- A self-taught front end developer from Mexico, programming worldwide. -->
+      A Web Developer from Mexico, <br /> programming worldwide.
+    </span>
+  </div>
+</header>
+
+<section></section>
