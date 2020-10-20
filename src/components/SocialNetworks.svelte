69b4a86a4307fb719c229d01bc13c90aed136840
papaponmx <jaime.rios.dev@outlook.com>
2020-10-19 23:28:11 -0500

    Created SocialNetworks component.

---
+++ src/components/SocialNetworks.svelte
@@ -0,0 +1,89 @@
+<style>
+  .social-networks--wrapper {
+    grid-column: 1 / 10;
+    grid-row-start: 10;
+  }
+
+  section.social-networks--wrapper {
+    display: flex;
+    justify-content: flex-start;
+    align-items: flex-start;
+    flex-direction: column;
+    max-width: 500px;
+  }
+
+  img {
+    width: calc(var(--spacing-unit) * 2);
+  }
+
+  ul {
+    display: flex;
+    justify-content: space-evenly;
+    padding-left: 0;
+    list-style: none;
+    max-width: 500px;
+    min-width: 100%;
+  }
+
+  li a {
+    align-items: center;
+    display: flex;
+    flex-direction: column;
+    gap: calc(var(--spacing-unit) * 1);
+    justify-content: space-between;
+    color: var(--mirage-color);
+  }
+
+  /* TODO: Animate hover states */
+</style>
+
+<section class="social-networks--wrapper">
+  <p>You find me online on the following social networks</p>
+  <ul>
+    {#each socialNetworks as socialNetwork}
+      <li>
+        <a
+          href="{socialNetwork.profileUrl}"
+          target="_blank"
+          title="{socialNetwork.name}"
+          rel="noreferrer nofollow"
+        >
+          <img
+            alt="{`${socialNetwork.name} brand logo`}"
+            role="figure"
+            src="{`images/${socialNetwork.name.toLowerCase()}.svg`}"
+          />
+        </a>
+      </li>
+    {/each}
+  </ul>
+</section>
+
+<script>
+  const socialNetworks = [
+    {
+      name: 'Dev',
+      profileUrl: 'https://dev.to/papaponmx',
+    },
+    {
+      name: 'Github',
+      profileUrl: 'http://github.com/papaponmx',
+    },
+    {
+      name: 'Dribbble',
+      profileUrl: 'http://dribbble.com/papaponmx',
+    },
+    {
+      name: 'Twitter',
+      profileUrl: 'https://twitter.com/papaponmx',
+    },
+    {
+      name: 'LinkedIn',
+      profileUrl: 'https://www.linkedin.com/in/papaponmx/',
+    },
+    {
+      name: 'Reddit',
+      profileUrl: 'https://www.reddit.com/user/papaponmx',
+    },
+  ];
+</script>
--- src/routes/contact/index.svelte
+++ src/routes/contact/index.svelte
@@ -1,41 +1,8 @@
 <style>
-  section.social-networks--wrapper {
-    display: flex;
-    justify-content: flex-start;
-    align-items: flex-start;
-    flex-direction: column;
-    max-width: 500px;
-  }
-
   section.contact-page--wrapper {
     display: grid;
     grid-template-columns: repeat(10, 10vw);
     grid-template-rows: repeat(12, 10vh);
-  }
-  img {
-    width: calc(var(--spacing-unit) * 2);
-  }
-
-  ul {
-    display: flex;
-    justify-content: space-evenly;
-    padding-left: 0;
-    list-style: none;
-    max-width: 500px;
-    min-width: 100%;
-  }
-  li a {
-    align-items: center;
-    display: flex;
-    flex-direction: column;
-    gap: calc(var(--spacing-unit) * 1);
-    justify-content: space-between;
-    color: var(--mirage-color);
-  }
-
-  li:hover span,
-  li:focus span {
-    display: block;
   }
 
   h1 {
@@ -45,8 +12,6 @@
     grid-column: 1 / 10;
     grid-row: 1 / 5;
   }
-
-  /* TODO: Animate hover states */
 
   p {
     font-size: 2.5rem;
@@ -76,11 +41,6 @@
   .email {
     margin-left: 2rem;
   }
-
-  .social-networks--wrapper {
-    grid-column: 1 / 10;
-    grid-row-start: 10;
-  }
 </style>
 
 <svelte:head>
@@ -100,55 +60,9 @@
   >jaime.rios@hey.com</a>
 
   <!-- TODO: Move this into its own commponent -->
-  <section class="social-networks--wrapper">
-    <h3>You find me online on the following social networks</h3>
-    <ul>
-      {#each socialNetworks as socialNetwork}
-        <li>
-          <a
-            href="{socialNetwork.profileUrl}"
-            target="_blank"
-            title="{`Go to my ${socialNetwork.name}'s profile`}"
-            rel="noreferrer nofollow"
-          >
-            <img
-              alt="{`${socialNetwork.name} brand logo`}"
-              role="figure"
-              src="{`images/${socialNetwork.name.toLowerCase()}.svg`}"
-            />
-            <span>{socialNetwork.name}</span>
-          </a>
-        </li>
-      {/each}
-    </ul>
-  </section>
 </section>
+<SocialNetworks />
 
 <script>
-  const socialNetworks = [
-    {
-      name: 'Dev',
-      profileUrl: 'https://dev.to/papaponmx',
-    },
-    {
-      name: 'Github',
-      profileUrl: 'http://github.com/papaponmx',
-    },
-    {
-      name: 'Dribbble',
-      profileUrl: 'http://dribbble.com/papaponmx',
-    },
-    {
-      name: 'Twitter',
-      profileUrl: 'https://twitter.com/papaponmx',
-    },
-    {
-      name: 'LinkedIn',
-      profileUrl: 'https://www.linkedin.com/in/papaponmx/',
-    },
-    {
-      name: 'Reddit',
-      profileUrl: 'https://www.reddit.com/user/papaponmx',
-    },
-  ];
+  import SocialNetworks from '../../components/SocialNetworks.svelte';
 </script>
