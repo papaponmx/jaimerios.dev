519ea8f44e9024fea6c528155f7ad580021549ff
papaponmx <jaime.rios.dev@outlook.com>
2020-10-23 07:32:13 -0500

    Updated URL logic

--- src/sitemap.js
+++ src/sitemap.js
@@ -2,7 +2,8 @@ const fs = require('fs');
 const fetch = require('node-fetch');
 
 const URL = 'https://dev.to/api/articles?username=papaponmx';
-const BASE_URL = process.env.VERCEL_URL || 'localhost:3000';
+const BASE_URL =
+  process.env === 'development' ? 'localhost:3000' : process.env.VERCEL_URL;
 const pages = [''];
 
 fs.readdirSync('./src/routes').forEach(file => {
