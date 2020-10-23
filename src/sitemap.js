b2ceaae111380521de2132b689f45e2029f18a44
papaponmx <jaime.rios.dev@outlook.com>
2020-10-23 07:51:01 -0500

    PUBLIC_VERCEL_URL

--- src/sitemap.js
+++ src/sitemap.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const fetch = require('node-fetch');
 
 const URL = 'https://dev.to/api/articles?username=papaponmx';
-const BASE_URL = process.env.VERCEL_URL;
+const BASE_URL = process.env.PUBLIC_VERCEL_URL;
 const pages = [''];
 
 fs.readdirSync('./src/routes').forEach(file => {
