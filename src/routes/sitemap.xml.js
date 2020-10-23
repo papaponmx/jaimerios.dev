e0f262cc22b35471f0463b86355676acce3810c5
papaponmx <jaime.rios.dev@outlook.com>
2020-10-23 00:09:48 -0500

    Added sitemap.xml

--- src/routes/sitemap.xml.js
+++ src/routes/sitemap.xml.js
@@ -1,42 +1,54 @@
-// import posts from "./blog/_posts";
-// const fs = require('fs');
+const fs = require('fs');
+const fetch = require('node-fetch');
 
-// const BASE_URL = VERCEL_URL;
-// const pages = [""];
+const URL = 'https://dev.to/api/articles?username=papaponmx';
+const BASE_URL = process.env.VERCEL_URL || 'localhost:3000';
+const pages = [''];
 
-// fs.readdirSync("./src/routes").forEach(file => {
-//   file = file.split('.')[0];
-//   if (file.charAt(0) !== '_' && file !== "sitemap" && file !== "index") {
-//     pages.push(file);
-//   }
-// });
+fs.readdirSync('./src/routes').forEach(file => {
+  file = file.split('.')[0];
+  if (file.charAt(0) !== '_' && file !== 'sitemap' && file !== 'index') {
+    pages.push(file);
+  }
+});
 
-// const render = (pages, posts) => `<?xml version="1.0" encoding="UTF-8" ?>
-// <urlset xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
-//   ${pages
-//     .map(
-//       page => `
-//     <url><loc>${BASE_URL}/${page}</loc><priority>0.85</priority></url>
-//   `
-//     )
-//     .join("\n")}
-//   ${posts
-//     .map(
-//       post => `
-//     <url>
-//       <loc>${BASE_URL}/blog/${post.slug}</loc>
-//       <priority>0.69</priority>
-//     </url>
-//   `
-//     )
-//     .join("\n")}
-// </urlset>
-// `;
+const render = (pages, posts) => `<?xml version="1.0" encoding="UTF-8" ?>
+<urlset xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+  ${pages
+    .map(
+      page => `
+    <url><loc>${BASE_URL}/${page}</loc><priority>0.85</priority></url>
+  `
+    )
+    .join('\n')}
+      ${
+        posts.length > 0 &&
+        posts
+          .map(
+            post => `
+    <url>
+      <loc>${BASE_URL}/blog/${post.slug}</loc>
+      <priority>0.69</priority>
+    </url>
+  `
+          )
+          .join('\n')
+      }
+</urlset>
+`;
 
-// export function get(req, res, next) {
-//   res.setHeader("Cache-Control", `max-age=0, s-max-age=${600}`); // 10 minutes
-//   res.setHeader("Content-Type", "application/rss+xml");
+export function get(req, res) {
+  res.setHeader('Cache-Control', `max-age=0, s-max-age=${600}`); // 10 minutes
+  res.setHeader('Content-Type', 'application/rss+xml');
 
-//   const sitemap = render(pages, posts);
-//   res.end(sitemap);
-// }
+  fetch(URL, {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then(r => r.json())
+    .then(posts => {
+      const sitemap = render(pages, posts);
+      res.end(sitemap);
+    });
+}
