859169535da2b451991c34c80aefff45d8488e75
papaponmx <jaime.rios.dev@outlook.com>
2020-10-27 14:03:51 -0600

    Updated general layout.

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,10 +1,8 @@
 <style lang="scss">
   @import '../styles/_variables';
   main {
-    background-color: white;
     box-sizing: border-box;
-    min-height: var(--main-height);
-    padding: 2em;
+    padding: 0 1rem;
   }
 
   /* .blog {
@@ -18,6 +16,50 @@
   .wrapper {
     display: flex;
     flex-direction: column;
+  }
+
+  /**
+   * Styles for Vertical Rhythmm
+   **/
+
+  :global(body) {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
+      Oxygen-Sans, Ubuntu, Cantarell, 'Helvetica Neue', sans-serif;
+    font-weight: 400;
+    color: #000000;
+  }
+
+  :global(p) {
+    margin-bottom: 1rem;
+  }
+
+  :global(h1, h2, h3, h4, h5) {
+    margin: 3rem 0 1.38rem;
+  }
+
+  :global(h1) {
+    margin-top: 0;
+    font-size: 7.594rem;
+  }
+
+  :global(h2) {
+    font-size: 5.063rem;
+  }
+
+  :global(h3) {
+    font-size: 3.375rem;
+  }
+
+  :global(h4) {
+    font-size: 2.25rem;
+  }
+
+  :global(h5) {
+    font-size: 1.5rem;
+  }
+
+  :global(small, .text_small) {
+    font-size: 0.667rem;
   }
 </style>
 
